03901c1ec3170fa1526b050b85012de9f3360f9b
HungLV <viet.hungle@aircanada.ca>
2023-03-01 15:39:07 +0700

    feat: add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import AppProvider from "@/providers/app";
-import { Button } from "@/components/Elements";
-import { useNotificationStore } from "./stores/notifications";
 
 function App() {
-  const { addNotification } = useNotificationStore();
   return (
     <AppProvider>
-      <Button
-        onClick={() => {
-          addNotification({
-            title: "The Noti",
-            message: "Notification",
-            type: "success"
-          })
-        }}
-      >Add Noti</Button>
     </AppProvider>
   )
 }
---
+++ src/components/Form/index.ts
@@ -0,0 +1,5 @@
+export * from "./Form";
+export * from "./FieldWarpper";
+export * from "./InputField";
+export * from "./SelectField";
+export * from "./TextareaField";
